c3f048015248291358a82d91b993b681826527d3
thamruicong <e0726692@u.nus.edu>
2022-08-18 19:00:20 +0800

    checklist

--- src/Pages/Createachecklistinreact/Createachecklistinreact.js
+++ src/Pages/Createachecklistinreact/Createachecklistinreact.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../mainstyle.css';
+import './Createachecklistinreact.css';
 
 function Createachecklistinreact() {
     const [data, setData] = useState([]);
@@ -10,18 +11,61 @@ function Createachecklistinreact() {
         // Load word data
         axios.get('https://catfact.ninja/breeds')
             .then(res => {
-                setData(res.data.data);
+                setData(res.data.data.map(x => {
+                    return {"breed": x.breed, "checked": false};
+                }));
             })
             .catch(err => {
                 console.log(err);
             })
+        
+        console.log(data);
     }, []);
+
+    const renderItem = (item, index) => {
+        return (
+            <div key={index} className='item'>
+                <input 
+                    type="checkbox" 
+                    value={data[index].checked} 
+                    onClick={() => {
+                        const newData = [...data]
+                        newData[index].checked = !data[index].checked
+
+                        setData(newData);
+                    }}
+                />
+                {item.breed}
+            </div>
+        );
+    };
+
+    const renderCheckedItem = (item) => {
+        return (
+            <>
+                {
+                    item.checked && <p>{item.breed}</p>
+                }
+            </>
+        );
+    }
 
     return (
         <div className='container'>
             <h1>Create A Checklist In React</h1>
             <Link to='/home' className="App-link">Home</Link>
             <br />
+
+            <div className='body'>
+                <div className='left'>
+                    {data.map(renderItem)}
+                </div>
+
+                <div className='right'>
+                    <p>List of checked items:</p>
+                    {data.map(renderCheckedItem)}
+                </div>
+            </div>
         </div>
     );
 }
